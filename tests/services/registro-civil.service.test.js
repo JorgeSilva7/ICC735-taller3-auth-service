1c6e602c38c88a732c36b46251f174f1aea58a9e
Jorge Silva <14844897+JorgeSilva7@users.noreply.github.com>
2023-06-17 09:13:22 -0400

    validation code default in dev

--- tests/services/registro-civil.service.test.js
+++ tests/services/registro-civil.service.test.js
@@ -3,20 +3,20 @@ import { HTTPError } from "../../src/helpers/error.helper.js";
 import { getCriminalRecords } from "../../src/services/registro-civil.service.js";
 
 describe("getCriminalRecords", () => {
-	it("should return a criminal records quantity=1 when request with 111111111", async () => {
+	it("should return a criminal records quantity=0 when request with 111111111", async () => {
 		const rut = "111111111";
 
 		const result = await getCriminalRecords(rut);
 
-		expect(result.quantity).toBe(1);
+		expect(result.quantity).toBe(0);
 	});
 
-	it("should return a criminal records quantity=0 when request with 777777777", async () => {
+	it("should return a criminal records quantity=1 when request with 777777777", async () => {
 		const rut = "777777777";
 
 		const result = await getCriminalRecords(rut);
 
-		expect(result.quantity).toBe(0);
+		expect(result.quantity).toBe(1);
 	});
 
 	it("should return a 404 when rut is empty", async () => {
