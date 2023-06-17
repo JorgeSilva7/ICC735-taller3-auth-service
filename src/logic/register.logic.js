c3ff302e126f508b8a2d8fe9baaf15b338ee076b
Jorge Silva <14844897+JorgeSilva7@users.noreply.github.com>
2023-06-17 08:35:47 -0400

    validation code default in dev

--- src/logic/register.logic.js
+++ src/logic/register.logic.js
@@ -8,12 +8,14 @@ import validateEmail from "../emails/validate.email.js";
 import { generateToken } from "../helpers/jwt.helper.js";
 import { getCriminalRecords } from "../services/registro-civil.service.js";
 
+const VALIDATION_DEV_DEFAULT = "asd123";
+
 const {
 	NOTIFICATIONS_SERVICE: {
 		SUBJECTS: { VALIDATE },
 	},
 	NODE_ENV,
-	ENVS: { local },
+	ENVS: { local, dev },
 } = environment;
 
 const randomizeConfig = {
@@ -63,13 +65,13 @@ async function checkIfUserAlreadyExists({ email, rut }) {
  * @returns {string} JWT
  */
 function generateCodeToken() {
-	const validationCode = randomize(
-		randomizeConfig.pattern,
-		randomizeConfig.length
-	);
+	let validationCode = null;
+	if (NODE_ENV !== dev) {
+		validationCode = randomize(randomizeConfig.pattern, randomizeConfig.length);
+	}
 
 	const codeToken = generateToken({
-		data: { code: validationCode },
+		data: { code: validationCode || VALIDATION_DEV_DEFAULT },
 		expiresIn: "15m",
 	});
 
