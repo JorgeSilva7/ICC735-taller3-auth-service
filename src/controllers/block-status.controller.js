c1ec8cd9d00c91f809cb98eeb594f85559d69077
Jorge Silva <14844897+JorgeSilva7@users.noreply.github.com>
2023-06-17 11:53:02 -0400

    change block status added

--- src/controllers/block-status.controller.js
+++ src/controllers/block-status.controller.js
@@ -5,7 +5,7 @@ async function changeBlockStatus(req, res) {
 	try {
 		const response = await blockStatusLogic({ userId: req.userId });
 
-		return res.status(200).json({ response });
+		return res.status(200).json(response);
 	} catch (error) {
 		return returnErrorResponse(error, res);
 	}
